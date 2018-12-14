6ca0f9f23899858743a9dc91c2b1ace761470532
RainNightneu <31750737+RainNightneu@users.noreply.github.com>
2018-12-14 16:29:06 -0500

    Add files via upload

---
+++ SqlGenerator-master/SqlGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SqlGenerator")]
+[assembly: AssemblyProduct("SqlGenerator")]
+[assembly: AssemblyDescription("Generate SQL statements from common input sources")]
+[assembly: AssemblyCompany("Jakob Lithner")]
+[assembly: AssemblyCopyright("Created by Jakob Lithner 2013-2016, Open Source")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+
+// Version: Major.Minor.Build.Revision
+// Note:    Build and Revision must be automatically generated to make the BuildDate property work!
+[assembly: AssemblyVersion("1.6.*")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
+
+
+// File versions
+// ========================================================================================================
+// 1.6		Allow connections to be edited in application.
+// 1.5		Parse table schema from SQL Server and create a mapping scenario.
+// 1.4		Remove settings file.
+// 1.3		Correct parsing of decimal values.
+// 1.2		Corrected parsing of rows with empty values. Allowed types to be surrounded by brackets. Allowed NULL values to be stated explicitly. Added validation of expected delimiter.
+// 1.1		Added version checker, clear button, splitter and DelimiterType.
+// 1.0		First distributed version.
